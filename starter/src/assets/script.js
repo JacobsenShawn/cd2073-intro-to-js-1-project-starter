4eb754a7ace179ce5c77616633da0e02ece1ff51
Shawn Jacobsen <5thkey1@gmail.com>
2024-08-29 20:03:22 -0400

    learning

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,13 +1,42 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+let products =[];
+console.log(products);
 
 /* Create 3 or more product objects using object literal notation 
-   Each product should include five properties
-   - name: name of product (string)
+   Each product should include five properties */
+   /*
+   - name: name of product (string) 
    - price: price of product (number)
    - quantity: quantity in cart should start at zero (number)
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
+
+   const fruit1 ={
+   name: 'Cherry' ,
+   price: 3,
+   quantity: 0,
+   product_id: 1,
+   image:(href="src/images/cherry.jpg")
+   }
+   console.log(fruit1)
+   const fruit2 ={
+    name: 'Orange' ,
+    price: 2,
+    quantity: 0,
+    product_id: 2,
+    image:(href="src/images/orange.jpg")
+    }
+    console.log(fruit1)
+    const fruit3 ={
+      name: 'Strawberry' ,
+      price: 2,
+      quantity: 0,
+      product_id: 3,
+      image:(href="src/images/strawberry.jpg")
+      }
+      console.log(fruit1)
+   /*
 
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
@@ -16,44 +45,63 @@
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
+let cart = [];
+
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
+function addProductToCart(product_Id){
 
+}
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
-
+function increaseQuantity(product_Id){
+  
+}
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
-
+function decreaseQuantity(product_Id){
+  
+}
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
-
+function removeProductFromCart(product_Id){
+  
+}
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total cost of all products
   - cartTotal should return the total cost of the products in the cart
   Hint: price and quantity can be used to determine total cost
 */
-
+function cartTotal(product_Id){
+  
+}
 /* Create a function called emptyCart that empties the products from the cart */
-
+function emptyCart(product_Id){
+  
+}
 /* Create a function named pay that takes in an amount as an argument
   - amount is the money paid by customer
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
   Hint: cartTotal function gives us cost of all the products in the cart  
 */
+function pay(product_Id){
+  
+}
+
+
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
@@ -62,8 +110,8 @@
    To fully complete this project, it is expected that all tests pass.
    Run the following command in terminal to run tests
    npm run test
-*/
 
+/*  Un comment to test code 
 module.exports = {
    products,
    cart,
@@ -75,5 +123,7 @@ module.exports = {
    pay, 
    emptyCart,
    /* Uncomment the following line if completing the currency converter bonus */
-   // currency
-}
+   /*
+   // currency 
+}  
+*/ 
